4277a0356fad99e1d6ae7e42e700ad98df2b7cf8
reim-weib-hr <hrrmanager985@gmail.com>
2024-02-29 00:51:56 -0500

    gg

--- src/index.js
+++ src/index.js
@@ -8,10 +8,6 @@ import { InitSpin } from './initial-spin/initial-spin';
 import { SolutionTwo } from './solu1';
 
 
-// const General = React.lazy(()=> import('./App'));
-
-// const Solution = React.lazy(()=> import('./solu'));
-
 ReactDOM.createRoot(document.querySelector('#root')).render(
   <React.StrictMode>
     <Suspense fallback={< InitSpin /> }>
