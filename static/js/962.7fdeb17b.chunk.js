a63b49eca61e3b3ff3aa016a35e46e597320e452
reim-weib-hr <hrrmanager985@gmail.com>
2024-02-29 00:37:11 -0500

    Updates

---
+++ static/js/962.7fdeb17b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfreegenpg=self.webpackChunkfreegenpg||[]).push([[962],{962:(e,a,r)=>{r.d(a,{_5Q:()=>n});var t=r(441);function n(e){return(0,t.k5)({tag:"svg",attr:{viewBox:"0 0 20 20",fill:"currentColor","aria-hidden":"true"},child:[{tag:"path",attr:{d:"M3 10a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zM8.5 10a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zM15.5 8.5a1.5 1.5 0 100 3 1.5 1.5 0 000-3z"},child:[]}]})(e)}}}]);
+//# sourceMappingURL=962.7fdeb17b.chunk.js.map
